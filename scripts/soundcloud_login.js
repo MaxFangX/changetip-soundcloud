451d7b0f2ad56172a9ee44f4cfa868c0ba6a8467
MaxFangX <maxfangx@gmail.com>
2015-01-02 01:10:36 -0600

    Script is able to access popup

--- scripts/soundcloud_login.js
+++ scripts/soundcloud_login.js
@@ -7,25 +7,22 @@ var casper = require('casper').create({
 });
 
 casper.start('http://soundcloud.com', function(){
-    console.log("Got to SoundCloud homepage");
+    console.log("**Got to SoundCloud homepage.");
     console.log("Title: "+ this.getTitle());
     
 });
 casper.then(function() {
-    console.log("Clicking login button");
+    console.log("**Clicked login button.");
     this.click('.header__login');
 });
 
-casper.then(function(){
-    console.log("Running timeout");
-    console.log("Popups length");
-    console.log(this.popups.length);
-})
-// casper.waitForPopup(/connect/, function() {
-//     console.log('At popup, title is: ');
-//     console.log(this.getTitle());
-//     console.log(casper.popups);
-// });
+casper.waitForPopup(/connect\?/, function() {
+    console.log('**Loaded login popup.');
+    console.log("casper.popups length: " + casper.popups.length);
+    console.log("casper.popups[0]: " + casper.popups[0]);
+}, function() {
+    console.log("Waiting for popup timed out at 15 seconds.");
+}, 15000);
 casper.run();
 
 
