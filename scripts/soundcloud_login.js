c452f83344756af35b2391013268f4f0a41be81a
MaxFangX <maxfangx@gmail.com>
2015-01-03 00:20:49 -0600

    Finally it can detect captchas

--- scripts/soundcloud_login.js
+++ scripts/soundcloud_login.js
@@ -1,6 +1,7 @@
 var system = require('system');
 var user = system.env['CHANGETIP_BOT_USER'];
 var pass = system.env['CHANGETIP_BOT_PASS'];
+
 //Make __utils__ accessible
 javascript:(function(){void(function(){if(!document.getElementById('CasperUtils')){var CasperUtils=document.createElement('script');CasperUtils.id='CasperUtils';CasperUtils.src='https://rawgit.com/n1k0/casperjs/master/modules/clientutils.js';document.documentElement.appendChild(CasperUtils);var interval=setInterval(function(){if(typeof ClientUtils==='function'){window.__utils__=new window.ClientUtils();clearInterval(interval);}},50);}}());})();
 
@@ -9,7 +10,7 @@ var casper = require('casper').create({
         loadImages: false,
         loadPlugins: false,
     },
-    verbose: true,
+    verbose: false,
     logLevel: "debug"
 });
 
@@ -48,9 +49,7 @@ casper.withPopup(/connect\?/, function() {
     console.log("Printing username form value");
     console.log(this.evaluate(function() {
         return document.querySelector('#username').value;
-    }));
-    //this.capture('popup111'+Math.random()+'.png');
-    
+    }));    
 });
 
 casper.wait(3000, function() {});
@@ -60,33 +59,38 @@ casper.withPopup(/connect\?/, function() {
     if(this.exists('#authorize')){
         console.log("Login button exists");
     };
+    //this.capture('000beforesubmit.png')
     this.click('#authorize');
 });
 
-casper.wait(3000, function(){
-    casper.then(function() {
-        this.capture('mainpage'+Math.random()+'.png');
-        console.log("POPUPS.LENGTH: "+ casper.popups.length);
-    })
-    casper.withPopup(/.*/, function() {
-        this.capture('popup222'+Math.random()+'.png');
-        console.log(this.evaluate(function() {
-            return document.title;
-        }))
-        console.log(this.evaluate(function() {
-            return document.querySelector('#recaptcha_area');
-        }))
-        if(casper.exists('#recaptcha_area')){
-            throw new Error("Ran into recaptcha, use a proxy.");
-        }
-        else{
-            console.log("Logged in successfully!");
-            console.log("Popups length: " + this.popups.length)
-        }  
-    })
-});
+casper.wait(3000, function(){});
 
 casper.then(function() {
+    //this.capture('111mainpage.png');
+    console.log("POPUPS.LENGTH: "+ casper.popups.length);
+})
+casper.wait(3000, function(){});
+
+casper.withPopup(/.*/, function() {
+    //this.capture('222popup.png');
+    console.log(this.evaluate(function() {
+        return document.title;
+    }))
+    console.log(this.evaluate(function() {
+        return document.querySelector('#recaptcha_area');
+    }))
+    if(casper.exists('#recaptcha_area')){
+        throw new Error("Ran into recaptcha, use a proxy.");
+    }
+    else{
+        console.log("****Logged in successfully!");
+        console.log("Popups length: " + this.popups.length)
+    }  
+})
+
+
+casper.then(function() {
+    this.capture('333final.png')
     console.log(this.evaluate(function() {
         return document.title;
     }))
