807b75be45458d8187ed0c16f46775668fd03b09
MaxFangX <maxfangx@gmail.com>
2015-01-07 11:28:24 -0600

    Str method to user model, changed var names

--- soundcloud/models.py
+++ soundcloud/models.py
@@ -4,8 +4,11 @@ from django.db import models
 class SoundCloudUser(models.Model):
 
     name = models.CharField(max_length=50, db_index=True)
-    user_email = models.CharField(max_length=254)
+    email = models.CharField(max_length=254)
     #TODO verify the max_length of SoundCloud urls
-    user_url = models.CharField(max_length=30, unique=True)
+    url = models.CharField(max_length=30, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+    	return "User '"+self.name+"' at soundcloud.com/" + self.url + " with email'"+ self.email + "', created at: " + self.created_at
+    	
