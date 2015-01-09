7067d25a2bc494772bd0d535aaf73855ce89d722
MaxFangX <maxfangx@gmail.com>
2015-01-09 11:10:43 -0600

    soundcloud/__status in urls.py

--- soundcloud/urls.py
+++ soundcloud/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('',
     url(r'^soundcloud/command-webhook$', 'soundcloud.views.command_webhook'),
-    url(r'^$', 'soundcloud.views.home'),
+    url(r'^soundcloud/__status', 'soundcloud.views.home'),
 )
