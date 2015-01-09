fdce712c9106c99f85edcd05fcd79258651e8c6a
MaxFangX <maxfangx@gmail.com>
2015-01-09 11:12:17 -0600

    Remove unneeded scripts

--- scripts/environment_variables.js
+++
@@ -1,6 +0,0 @@
-var system = require('system');
-var bot_key = system.env['CHANGETIP_BOT_API_KEY'];
-var dev_key = system.env['CHANGETIP_API_KEY'];
-var proxy1 = system.env['PROXY_1'];
-var proxy2 = system.env['PROXY_2'];
-var proxy3 = system.env['PROXY_3'];
