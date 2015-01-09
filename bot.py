8d77793856bb2d195f166a32612b135d9bbb3bb7
MaxFangX <maxfangx@gmail.com>
2015-01-08 21:44:41 -0600

    Bot that overrides basebot

--- bot.py
+++ bot.py
@@ -1,12 +1,67 @@
 import json
 import subprocess
+import os
 
 from changetip.bots.base import BaseBot
 
 
 class SoundCloudBot(BaseBot):
-	def check_for_new_tips(self):
-		pass
+	changetip_api_key = os.getenv("CHANGETIP_API_KEY", "fake_key")
+	assert changetip_api_key != "fake_key", "Need to set CHANGETIP_API_KEY environment variable"
+
+    username = "maxtipbot"  # username on the site
+    prefix = "@"
+    last_context_uid = None
+    proxy = None
+
+    # How many seconds the bot runner should wait before checking for new tips
+    new_tip_check_delay = 30
+
+
+    def dupecheck(self, context_uid):
+        """ Check locally for duplicates before submitting
+        Should raise a DuplicateTipException if duplicate is found
+        """
+        pass #TODO
+
+    def check_for_new_tips(self, last):
+        """ Poll the site for new tips. Expected to return an array of tips, in the format passed to send_tip """
+        pass #TODO
+    
+    # def send_tip(self, sender, receiver, message, context_uid, meta):
+
+    def deliver_tip_response(self, tx):
+        """ Does the work to post the response to the thread on the site. Returns True or Exception """
+        raise NotImplementedError
+
+    #def deliver_tip_confirmation(self, tx):
+
+    def invite_new_user(self, sender, **kwargs):
+        """ Invite the sender to create an account on ChangeTip
+        :param **kwargs:
+        """
+        pass #TODO
+
+    def missing_amount_message(self, tx, tiplike_text, process=True):
+        """ How to interact with the user when there is no amount parsed
+        """
+        pass #TODO
+
+    def send_tip_reminder(self, tx):
+        """ A reminder to the receiver that their tip is about to expire """
+        pass #TODO
+
+    def on_over_tip_limit(self, tx, limit, process=True):
+        """ How to interact with the user when there is an tip attempt over the limit
+        """
+        pass #TODO
+
+    # def get_api_url(self, path):
+
+    # def get_mentions(self, message):
+
+    # def get_sibling_tips(self, parent_id):
+
 
 #TESTING AREA BELOW
 
