43726fc5e968e846a9e0608a943d3d963415d4c6
MaxFangX <maxfangx@gmail.com>
2015-01-12 21:20:42 -0800

    Handle successful tip

--- src/run.py
+++ src/run.py
@@ -6,6 +6,9 @@ assert SOUNDCLOUD_CLIENT_ID != "fake_client_id", "Need to set SOUNDCLOUD_CLIENT_
 SOUNDCLOUD_CLIENT_SECRET = os.getenv("SOUNDCLOUD_CLIENT_SECRET", "fake_client_secret")
 assert SOUNDCLOUD_CLIENT_SECRET != "fake_client_secret", "Need to set SOUNDCLOUD_CLIENT_SECRET environment variable"
 
+#Reference values
+info_url = "https://www.changetip.com/tip-online/soundcloud" 
+#TODO make sure this is supported by the site
 
 # GET TIPS
 print("Running script")
@@ -26,14 +29,23 @@ for index in tips:
 	out = ""
 	if response.get("error_code") == "invalid_sender":
 		out = "Handling invalid sender %s" % tip['sender']
-		bot.invite_new_user(tip['sender'])
+		bot.invite_new_user(tip['sender']) #TODO implement
 	elif reponse.get("error_code") == "duplicate_context_uid":
 		out = "Handling duplicate tip %s" % tip['context_uid']
-		pass 
+		pass
+	elif response.get("state") in ["ok", "accepted"] #TODO 
+		_tip = response['tip']
+		print("Response Tip: " + str(response['tip'])) #test
+		if _tip['status'] == "out for delivery":
+			out = "The tip for %s is out for delivery. %s needs to collect their tip by connecting their ChangeTip account to SoundCloud at %s" % (_tip['amount_display'], 
+									_tip['receiver'], 
+									info_url)
+		elif _tip['status'] == "finished":
+			out = "The tip has been delivered, %s has been added to %s's ChangeTip wallet." % (_tip['amount_display'],
+									_tip['receiver'])
 
-
-
-	print(out)
+	print("Tip processed. Output: ") #test
+	print(out) #TODO make this a return value for Celery
 
 response = bot.send_tip(**tip_data)
 out = ""
