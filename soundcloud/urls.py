37cfce6bbcb8aa1d3f864011f09145c853f52795
MaxFangX <maxfangx@gmail.com>
2015-01-13 00:18:30 -0800

    Delete django project and celery, reorganize files

--- soundcloud/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('',
-    url(r'^soundcloud/command-webhook$', 'soundcloud.views.command_webhook'),
-    url(r'^soundcloud/__status', 'soundcloud.views.home'),
-)
