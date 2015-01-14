fb8f819a151dd101852cc6375fff65ca63022eeb
MaxFangX <maxfangx@gmail.com>
2015-01-13 23:24:59 -0800

    Remove verbose outputs, better testing in run.py

--- src/run.py
+++ src/run.py
@@ -14,7 +14,7 @@ info_url = "https://www.changetip.com/tip-online/soundcloud"
 print("Running script")
 bot = SoundCloudBot()
 tips = bot.check_for_new_tips(None)
-print("Tips: " + str(tips))
+print("Tips: " + str(len(tips))) #test
 
 # SUBMIT TIPS
 print("Submitting tips")
@@ -22,7 +22,9 @@ print("Submitting tips")
 #Loop through dictionary of tips and submit each
 for index in tips:
 	tip = tips[index]
-	print(str(tip)) #test
+	print("Testing tip from %s to %s with message'%s'" % (tip['sender'], 
+														tip['receiver'],
+														tip['message']))
 
 	#TODO use SoundCloudBot.dupe_check here
 	response = bot.send_tip(**tip)
@@ -35,7 +37,7 @@ for index in tips:
 		pass
 	elif response.get("state") in ["ok", "accepted"]: #TODO 
 		_tip = response['tip']
-		print("Response Tip: " + str(response['tip'])) #test
+		#print("Response Tip: " + str(response['tip'])) #test
 		if _tip['status'] == "out for delivery":
 			out = "The tip for %s is out for delivery. %s needs to collect their tip by connecting their ChangeTip account to SoundCloud at %s" % (_tip['amount_display'], 
 									_tip['receiver'], 
@@ -43,9 +45,8 @@ for index in tips:
 		elif _tip['status'] == "finished":
 			out = "The tip has been delivered, %s has been added to %s's ChangeTip wallet." % (_tip['amount_display'],
 									_tip['receiver'])
-
+	else:
+		out = "Did not hit any cases. Tip status unknown."
 	print("Tip processed. Output: ") #test
 	print(out) #TODO make this a return value for Celery
 
-response = bot.send_tip(**tip_data)
-out = ""
